534f951709af58765e3ec2ef2a9e6b276da73a31
Riyadh <ryad1067@hotmail.com>
2023-05-08 03:23:58 +0300

    Updated with modified constructors

--- Tile.cpp
+++ Tile.cpp
@@ -3,14 +3,62 @@
 //
 
 #include "Tile.hpp"
+#include "Game.hpp"
 
 
-
-Tile::Tile(Shapes s) {
-    m_shape = pick_shape(s);
-    m_shape.setPosition(150,500);
+Tile::Tile(): Collidable() {
+    m_shape.setPosition(0,0);
+    x = m_shape.getPosition().x;
+    y = m_shape.getPosition().y;
+    Collidable::x = m_shape.getPosition().x;
+    Collidable::y = m_shape.getPosition().y;
 }
 
 Sprite Tile::draw() {
     return m_shape;
+}
+
+void Tile::setShape(Shapes s){
+    m_shape = pick_shape(s);
+    if(s == land_left || s == road_topLeft || s == road_bottomLeft)
+        m_shape.setPosition(0,0);
+    else if(s == land_right)
+        m_shape.setPosition(216,0); // win_w - thickness of right lane = 304 - 88 = 216
+    else if(s == road_topRight || s == road_bottomRight)
+        m_shape.setPosition(176,0); // win_w - road_w = 304 - 124 = 176
+    else if(s == corner_top_l || s == corner_bottom_l)
+        m_shape.setPosition(72,50);
+    else if(s == corner_top_r || s == corner_bottom_r)
+        m_shape.setPosition(216-32,50);
+
+
+
+    x = m_shape.getPosition().x;
+    y = m_shape.getPosition().y;
+
+    Collidable::x = m_shape.getPosition().x;
+    Collidable::y = m_shape.getPosition().y;
+    Collidable::w = m_shape.getLocalBounds().width;
+    Collidable::h = m_shape.getLocalBounds().height;
+}
+
+void Tile::move_d(float speed) {
+    move(0,speed);
+}
+
+void Tile::move(float xOff, float yOff) {
+    m_shape.move(xOff, yOff);
+    x = m_shape.getPosition().x;
+    y = m_shape.getPosition().y;
+    Collidable::x = m_shape.getPosition().x;
+    Collidable::y = m_shape.getPosition().y;
+}
+
+void Tile::updatePosition() {
+    if (y > WIN_H - TOL){
+        m_shape.setPosition(x,-WIN_H);
+        y = m_shape.getPosition().y;
+        Collidable::y = m_shape.getPosition().y;
+    }
+
 }
--- Tile.hpp
+++ Tile.hpp
@@ -6,18 +6,39 @@
 #define RIVER_RAID_TILE_HPP
 #include <SFML/Graphics.hpp>
 #include "Shapes.hpp"
+#include "Collidable.hpp"
 
 using namespace std;
 using namespace sf;
 
-class Tile {
-private:
+class Tile: public Collidable {
+protected:
     Sprite m_shape;
     float x{0};
     float y{0};
 public:
     float m_speed;
-    Tile(Shapes);
+    Tile();
+    Tile(Shapes s) : Collidable(), m_shape{pick_shape(s)} {
+        if(s == land_left || s == road_topLeft)
+            m_shape.setPosition(0,0);
+        else if(s == land_right)
+            m_shape.setPosition(216,0); // win_w - thickness of right lane = 304 - 88 = 216
+        else if(s == road_topRight || s == road_bottomRight)
+            m_shape.setPosition(176,0); // win_w - road_w = 304 - 124 = 176
+        else if(s == corner_top_l)
+            m_shape.setPosition(72,50);
+        else if(s == corner_top_r)
+            m_shape.setPosition(176,50);
+        Collidable::x = m_shape.getPosition().x;
+        Collidable::y = m_shape.getPosition().y;
+        Collidable::w = m_shape.getLocalBounds().width;
+        Collidable::h = m_shape.getLocalBounds().height;
+    }
+    void move_d(float speed);
+    void move(float xOff, float yOff);
+    void updatePosition();
+    void setShape(Shapes);
     Sprite draw();
 
 };
